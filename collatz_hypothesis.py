413ce55ce17766af78cdebd965115ee5b389c5bb
moe-hawk <95315540+moe-hawk@users.noreply.github.com>
2021-12-10 15:58:33 -0500

    Added file via upload

---
+++ collatz_hypothesis.py
@@ -0,0 +1,15 @@
+﻿steps=0
+c0=int(input("input c0:"))
+while c0>1:
+    if c0%2==0:
+        c0=c0/2
+        steps+=1
+        print(int(c0))
+    elif c0%2==1:
+        c0=3*c0+1
+        steps+=1
+        print(int(c0))
+print('steps =', steps)
+
+
+
